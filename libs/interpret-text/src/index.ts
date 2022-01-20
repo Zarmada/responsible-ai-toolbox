e3cda2b11aa4015b4a9b86aaf161edf4b8e041c9
Ilya Matiach <ilmat@microsoft.com>
2022-01-20 10:41:33 -0500

    add text interpretability dashboard UI

--- libs/interpret-text/src/index.ts
+++ libs/interpret-text/src/index.ts
@@ -1,4 +1,4 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-export * from "./lib/TextExplanationDashboard";
+export * from "./lib/TextExplanationDashboard/TextExplanationDashboard";
